8f2f55abe1655dcf8c0b5c437317d625de8395c9
Seacrest <paganseacrest@gmail.com>
2025-02-05 15:31:36 +0200

    fix tranactions

--- src/components/arrow/arrow.tsx
+++ src/components/arrow/arrow.tsx
@@ -5,6 +5,7 @@ import arr_up_chs from "assets/icons/arr_up_chs.svg"
 
 import arr_down_cr from "assets/icons/arr_down_cr.svg"
 import arr_up_cr from "assets/icons/arr_up_cr.svg"
+
 import { useSelector } from "react-redux";
 import { selectCreditsHints, selectPost, selectPosts } from "src/store/post_slice";
 import { selectUser } from "src/store/user_slice";
@@ -28,7 +29,6 @@ const Arrow: FC<Props> = ({kind}) => {
 
   useEffect(() => {
     const post = posts.find(post => post.id === currentId)!;
-
 
     if (currentId) {
       setFlag({
@@ -57,9 +57,9 @@ const Arrow: FC<Props> = ({kind}) => {
   return (
     <div className={styles.arr}>
       {kind === "chaos" ? (
-        user.chaos - flag.chaos < user.chaos ? <img src={arr_down_chs} alt="arr" /> : <img src={arr_up_chs} alt="arr" />
+        user.chaos - flag.chaos > user.chaos ? <img src={arr_down_chs} alt="arr" /> : <img src={arr_up_chs} alt="arr" />
       ) : (
-        user.credits - flag.cred < user.credits ? <img src={arr_down_cr} alt="arr" /> : <img src={arr_up_cr} alt="arr" />
+        user.credits - flag.cred > user.credits ? <img src={arr_down_cr} alt="arr" /> : <img src={arr_up_cr} alt="arr" />
       )}
     </div>
   );
